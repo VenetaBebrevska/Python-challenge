61a3ec57b2fa2fbcda638697bd6a42f0447908d8
unknown <veneta_sb@yahoo.com>
2019-10-12 12:05:47 -0500

    csv file

--- PyBank/Solved/main.py
+++ PyBank/Solved/main.py
@@ -22,6 +22,27 @@ with open(csvpath, newline='') as csvfile:
     for row in csvreader:
         print(row)
 
+# The total number of months included in the dataset (minus 1 for the header)
+with open(csvpath) as csvfile:
+    month_count = sum(1 for month in csvfile)-1
+    print(month_count if month_count else 'Empty')
+
+# The net total amount of "Profit/Losses" over the entire period
+# The average of the changes in "Profit/Losses" over the entire period
+#def average(X)
+    #length = len(X)
+    #total = 0.0
+    #for profit in X:
+        #total += profit
+    #return total / length
+
+    #print(average(X))    
+
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in losses (date and amount) over the entire period
+
+
+
 
 
         
